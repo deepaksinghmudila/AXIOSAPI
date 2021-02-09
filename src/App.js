b5d57fc91c38ca4941e9a8605a28e027a465497e
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-02-09 20:44:12 +0530

    PropType

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
  import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from "prop-types";
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -191,6 +192,25 @@ const Table = ({ list, onDismiss }) =>
     )}
   </div>
 
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
+/*  
+Table.propTypes = {
+  list: PropTypes.array.isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
+*/
+
 const Button = ({
   onClick,
   className = '',
@@ -203,6 +223,17 @@ const Button = ({
   >
     {children}
   </button>
+
+Button.defaultProps = {
+     className: '',
+};
+
+/*
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}; */
 
 export default App;
 
